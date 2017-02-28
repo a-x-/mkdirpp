58d7e7ed164f40c33949c08e1c2bb3c48cbcbe16
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2017-02-28 19:11:10 +0300

    Wrap promise. Remove sync support. Refactor code

--- index.js
+++ index.js
@@ -1,98 +1,75 @@
+/**
+ * @file
+ * Based on https://github.com/substack/node-mkdirp
+ */
+
 var path = require('path');
 var fs = require('fs');
 var _0777 = parseInt('0777', 8);
 
-module.exports = mkdirP.mkdirp = mkdirP.mkdirP = mkdirP;
+module.exports = mkdirp;
 
-function mkdirP (p, opts, f, made) {
-    if (typeof opts === 'function') {
-        f = opts;
-        opts = {};
-    }
-    else if (!opts || typeof opts !== 'object') {
-        opts = { mode: opts };
-    }
-    
-    var mode = opts.mode;
-    var xfs = opts.fs || fs;
-    
-    if (mode === undefined) {
-        mode = _0777 & (~process.umask());
-    }
-    if (!made) made = null;
-    
-    var cb = f || function () {};
-    p = path.resolve(p);
-    
-    xfs.mkdir(p, mode, function (er) {
-        if (!er) {
-            made = made || p;
-            return cb(null, made);
-        }
-        switch (er.code) {
-            case 'ENOENT':
-                mkdirP(path.dirname(p), opts, function (er, made) {
-                    if (er) cb(er, made);
-                    else mkdirP(p, opts, cb, made);
-                });
-                break;
+/**
+ * @param {String} p - path to create
+ *
+ * @param {Object|Number|undefined} opts
+ * @param {*} opts.fs
+ *
+ * @returns {Promise}
+ *
+ * @public
+ */
+function mkdirp (p, opts) {
+    var opts_ = {}; // PERF: don't change arguments
 
-            // In the case of any other error, just see if there's a dir
-            // there already.  If so, then hooray!  If not, then something
-            // is borked.
-            default:
-                xfs.stat(p, function (er2, stat) {
-                    // if the stat fails, then that's super weird.
-                    // let the original error be the failure reason.
-                    if (er2 || !stat.isDirectory()) cb(er, made)
-                    else cb(null, made);
-                });
-                break;
-        }
-    });
+    if (typeof opts === 'number') {
+        opts_ = { mode: opts };
+    }
+    else if (opts) {
+        Object.assign(opts_, opts);
+    }
+
+    opts_.mode = opts_.mode || _0777 & (~process.umask())
+
+    return _mkdirp(p, opts_);
 }
 
-mkdirP.sync = function sync (p, opts, made) {
-    if (!opts || typeof opts !== 'object') {
-        opts = { mode: opts };
-    }
-    
-    var mode = opts.mode;
+/**
+ * @param {String} p - path to create
+ *
+ * @param {Object|String|undefined} opts
+ * @param {*} opts.fs
+ * @param {Number} opts.mode
+ *
+ * @returns {Promise}
+ *
+ * @private
+ */
+function _mkdirp (p, opts) {
     var xfs = opts.fs || fs;
-    
-    if (mode === undefined) {
-        mode = _0777 & (~process.umask());
-    }
-    if (!made) made = null;
 
     p = path.resolve(p);
 
-    try {
-        xfs.mkdirSync(p, mode);
-        made = made || p;
-    }
-    catch (err0) {
-        switch (err0.code) {
-            case 'ENOENT' :
-                made = sync(path.dirname(p), opts, made);
-                sync(p, opts, made);
-                break;
+    return new Promise(function (res, rej) {
+        xfs.mkdir(p, opts.mode, function (er) {
+            if (!er) {
+                return res();
+            }
+            switch (er.code) {
+                case 'ENOENT':
+                    return _mkdirp(path.dirname(p), opts).then(function (er) {
+                        return _mkdirp(p, opts);
+                    });
+                    break;
 
-            // In the case of any other error, just see if there's a dir
-            // there already.  If so, then hooray!  If not, then something
-            // is borked.
-            default:
-                var stat;
-                try {
-                    stat = xfs.statSync(p);
-                }
-                catch (err1) {
-                    throw err0;
-                }
-                if (!stat.isDirectory()) throw err0;
-                break;
-        }
-    }
+                case 'EEXIST':
+                    return res();
+                    break;
 
-    return made;
-};
+                default:
+                    return rej();
+                    break;
+            }
+        });
+    });
+}
